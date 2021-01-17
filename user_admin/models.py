0697980ecabbfe849bb6134a6ab54c75f7d7ca99
Jahid <programmerjahid162@gmail.com>
2021-01-18 00:03:58 +0600

    test

--- user_admin/models.py
+++ user_admin/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.fields import DateTimeField
+import datetime
+from django.utils import timezone
 # Create your models here.
+
+class Notification(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    duration = models.DateField(null=True)
+    title= models.CharField(max_length=200,null=True,blank=True)
+    body = models.TextField(max_length=300,null=True,blank=True)
+    status = models.BooleanField(default=True)
+    
+    
+    def __str__(self):
+        return self.title
